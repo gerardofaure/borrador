29460924b304fa9c90b53b9377bc69d19d13311e
gerardofaure <gerardofaure@gmail.com>
2024-03-07 08:58:32 -0300

    carga inicial

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const operaciones = require("./operaciones.js");
+
+console.log (operaciones.suma(5,5));
+console.log (operaciones.resta(10,5));
+console.log (operaciones.multiplica(10,10));
+console.log (operaciones.divide(25,5));
